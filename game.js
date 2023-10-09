4d7834c13486d95fdfc0732dd16ea249072b0abb
Lef7Out <daveze727@gmail.com>
2023-10-09 20:06:30 +0100

    Create your game function

--- game.js
+++ game.js
@@ -23,3 +23,50 @@ function playerSelection(){
     }else
     console.log("Invalid Input");
 }
+
+function game() {
+    let user = playerSelection();
+    let comp = computerSelection();
+
+    if(user === comp){
+        console.log("It's a tie");
+    }
+    else if(user === "rock"){
+        if(comp === "paper"){
+            console.log("You lose!");
+            return "lose";
+        }else if(comp === "scissors"){
+        console.log("You win!");
+        return "win";
+    }else{
+    console.log("Its a tie Try Again!!")
+    return "tie"
+    }
+    }else if(user === "paper"){
+        if(comp === "rock"){
+            console.log("You win!");
+            return "win";
+    }else if(comp === "scissors"){
+        console.log("You lose!");
+        return "lose";
+    }else{
+        console.log("Its a Tie Try Again!");
+        return "tie";
+    }
+}else if(user === "scissors"){
+    if(comp === "rock"){
+        console.log("You lose!");
+        return "lose";
+    }else if(comp === "paper"){
+        console.log("You win!");
+        return "win";
+    }else{
+        console.log("Its a tie Try Again!");
+        return "tie";
+    }
+
+}else{
+    console.log("Enter either Rock Paper or Scissors")
+}
+}
+
